8a509576d62e8ef6d0f95444f8608efcceab66f7
amaury-soto <amaury.soto@globant.com>
2024-06-12 15:53:13 -0500

    initial settings react query

---
+++ src/issues/components/LabelPicker.tsx
@@ -0,0 +1,21 @@
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { useLabels } from "../hooks/useLabels";
+
+export const LabelPicker = () => {
+  const labelsQuery = useLabels();
+  if (labelsQuery.isLoading) return <LoadingIcon/>
+
+  return (
+    <div>
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className="badge rounded-pill m-1 label-picker"
+          style={{ border: `1px solid #${label.color}`, color:  `#${label.color}`}}
+        >
+          {label.name}
+        </span>
+      ))}
+    </div>
+  );
+};
---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+import { sleep } from "../../helpers/sleep";
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Label[]>("/labels");
+
+  console.log("data1", data);
+  return data;
+};
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    staleTime: 1000 * 60 * 60,
+  });
+  return labelsQuery;
+};
