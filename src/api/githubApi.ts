1be5a79e8ec44dc601d71cd7c30485001a561d69
amaury-soto <amaury.soto@globant.com>
2024-06-20 12:22:14 -0500

    added user token from github

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
-    Authorization:'Bearer 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'
+   // Authorization:'Bearer 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'
   },
 });
